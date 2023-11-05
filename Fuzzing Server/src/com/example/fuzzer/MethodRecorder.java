c21a6c003e1287c2228993c12be3a405108947ab
HumbleLearn3r <68717672+SioYooo@users.noreply.github.com>
2023-11-06 01:01:17 +1100

    update

--- Fuzzing Server/src/com/example/fuzzer/MethodRecorder.java
+++
@@ -1,7 +0,0 @@
-package com.example.fuzzer;
-public class MethodRecorder implements java.io.Serializable{
-    String className;
-    String methodName;
-    Object[] pars;
-    String res;
-}
--- Fuzzing Server/src/com/example/fuzzer/ObjectRecorder.java
+++
@@ -1,9 +0,0 @@
-package com.example.fuzzer;
-
-public class ObjectRecorder implements java.io.Serializable{
-    String objName;
-    int constructorIndex;
-    Object[] valArr;
-    // Object[] valArr = new Object[pars.length];
-
-}
